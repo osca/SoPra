a8c931be3db315e1dbcf6f2db01758bbd9a20ca7
Dennis Wiosna <dennis.wiosna@googlemail.com>
2012-02-27 15:52:47 +0100

    Auto Update

--- angebote/Angebotsverwaltung.java
+++ angebote/Angebotsverwaltung.java
@@ -1,14 +1,5 @@
 package angebote;
 
-import java.util.Vector;
-
-import angebote.typen.Angebot;
-
 public class Angebotsverwaltung {
 
-	private Vector<Angebot> angebote;
-	
-	public Vector<Kommentar> getKommentare(Angebot angebot) {
-		return angebot.kommentare;
-	}
 }
