66eb29aa8816ef853d586e953784ffa3bebf9754
Stephan Övermöhle <stephan@stephan-ThinkPad-T500.(none)>
2012-02-27 15:39:20 +0100

    Auto Update

--- accounts/Nachricht.java
+++ accounts/Nachricht.java
@@ -1,5 +1,16 @@
 package accounts;
 
 public class Nachricht {
-
+	private String text, betreff;
+	public String getText() {
+		return text;
+	}
+	public String getBetreff() {
+		return betreff;
+	}
+	public Nachricht(String subj, String txt){
+		text=txt;
+		betreff=subj;
+	}
+	
 }
