44b9c3fd9c6aacc8ebab8156811cb0d21607052e
J <Jay.Schneider@uni-muenster.de>
2012-02-27 15:48:52 +0100

    Auto Update

--- accounts/Accountverwaltung.java
+++ accounts/Accountverwaltung.java
@@ -1,5 +1,52 @@
 package accounts;
 
-public class Accountverwaltung {
+import java.util.Vector;
 
+public class Accountverwaltung {
+	
+	private Vector<Account> accounts = new Vector<Account>();
+
+	public void createKunde(String email, String name, String password) throws AlreadyInUseException{
+		if (!isFreeEmail(email) || !isFreeName(name))
+			throw new AlreadyInUseException();
+		accounts.add(new Kunde(email, name, password));
+	}
+
+	public void createAnbieter(String email, String name, String password) throws AlreadyInUseException{
+		if (!isFreeEmail(email) || !isFreeName(name))
+			throw new AlreadyInUseException();
+		accounts.add(new Kunde(email, name, password));
+	}
+	
+	public void createBetreiber(String email, String name, String password) throws AlreadyInUseException{
+		if (!isFreeEmail(email) || !isFreeName(name))
+			throw new AlreadyInUseException();
+		accounts.add(new Betreiber(email, name, password));
+	}
+	
+	public void setEnableAccount(Account acc, boolean enable){
+		acc.setGesperrt(enable);
+	}
+	
+	public void delAccount(Account acc){
+		boolean success = accounts.remove(acc);
+		if(!success)
+			return;		//TODO exception??
+	}
+	
+	private boolean isFreeEmail(String email) {
+		boolean result = true;
+		for(Account a : accounts)
+			if(a.getEmail().equals(email))
+				result = false;
+		return result;
+	}
+
+	private boolean isFreeName(String name) {
+		boolean result = true;
+		for(Account a : accounts)
+			if(a.getName().equals(name))
+				result = false;
+		return result;
+	}
 }
---
+++ accounts/AlreadyInUseException.java
@@ -0,0 +1,6 @@
+package accounts;
+
+public class AlreadyInUseException extends Exception {
+	private static final long serialVersionUID = 8058644619212255947L;
+
+}
