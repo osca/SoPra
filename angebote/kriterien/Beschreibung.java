c23dccb7a6c20231a692bb78cdbcf0cbd4c83720
Stephan Övermöhle <stephan@stephan-ThinkPad-T500.(none)>
2012-02-27 17:26:57 +0100

    Auto Update

---
+++ angebote/kriterien/Beschreibung.java
@@ -0,0 +1,11 @@
+package angebote.kriterien;
+//@author stephan
+import java.util.Vector;
+
+public class Beschreibung extends Kriterium{
+
+	Beschreibung(Vector<String> pwerte) throws IllegalArgumentException {
+		super(pwerte);
+	}
+
+}
