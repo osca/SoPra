c6835a473e5f11864c7022fe6feb95dbf3bc4e38
Dennis Wiosna <dennis.wiosna@googlemail.com>
2012-02-27 14:54:03 +0100

    Auto Update

--- angebote/Angebot.java
+++ angebote/Angebot.java
@@ -1,5 +1,14 @@
 package angebote;
 
-public class Angebot {
+import java.util.Date;
 
+public class Angebot {
+	
+	private static int anzahl = 0;
+	private int angebotsNummer,typ;
+	private String name;
+	private boolean auffindbar;
+	private double preis;
+	private Date[] daten;
+	
 }
