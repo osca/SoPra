91deca4604ad25d47cb41eeea084957b681a7424
Stephan Övermöhle <stephan@stephan-ThinkPad-T500.(none)>
2012-02-27 14:57:30 +0100

    Auto Update

--- accounts/Account.java
+++ accounts/Account.java
@@ -1,6 +1,55 @@
 package accounts;
 
+import java.util.Vector;
+
 public abstract class Account {
-	String email, name, password;
+	private String email, name, password;
+	protected boolean gesperrt = false;
+	private Vector<Nachricht> Posteingang;
 	
+	// GETTER und SETTER														//|
+	public String getEmail() {													//|
+		return email;															//|
+	}																			//|
+
+	public void setEmail(String email) {										//|
+		this.email = email;														//|
+	}																			//|
+
+	public String getName() {													//|
+		return name;															//|
+	}																			//|
+
+	public void setName(String name) {											//|
+		this.name = name;														//|
+	}																			//|
+	//Sollte noch ersetzt werden durch vernünftige Sicherheitsabfragen
+	public String getPassword() {												//|
+		return password;														//|
+	}																			//|
+
+	public void setPassword(String password) {									//|
+		this.password = password;												//|
+	}																			//|
+	//Password-getter-und Setter sind doof!!!
+	
+	
+	public boolean isGesperrt() {												//|
+		return gesperrt;														//|
+	}																			//|	
+
+	public void setGesperrt(boolean gesperrt) {									//|
+		this.gesperrt = gesperrt;												//|
+	}																			//|
+
+	public void addNachricht(Nachricht msg){
+		Posteingang.add(msg);
+	}
+	
+
+	public Account(String em, String nm, String pw){
+		email=em;
+		name =nm;
+		password=pw;
+	}
 }
--- accounts/Anbieter.java
+++ accounts/Anbieter.java
@@ -1,5 +1,12 @@
 package accounts;
 
-public class Anbieter {
+public class Anbieter extends Account{
+	
+	public Anbieter(String em, String nm, String pw) {
+		super(em, nm, pw);
+		gesperrt = true;
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
