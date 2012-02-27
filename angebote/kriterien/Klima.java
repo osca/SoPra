ef0d6234b2eb1c6595a23c82cbeee3dd16ea8659
Dennis Wiosna <dennis.wiosna@googlemail.com>
2012-02-27 16:36:17 +0100

    Auto Update

--- angebote/kriterien/Klima.java
+++ angebote/kriterien/Klima.java
@@ -1,10 +1,17 @@
 package angebote.kriterien;
 
+import java.util.Vector;
+
 public class Klima extends Kriterium{
-	Vector<String>[] klimata = {""};
 	
-	Klima(String[] pwerte) {
+	Vector<String> klimata = new Vector<String>();
+	
+	Klima(Vector<String> pwerte) {
 		super(pwerte);
+		
+		klimata.add("Sonnig");
+		klimata.add("");
+		
 		erlaubteWerte = klimata;
 	}
 }
