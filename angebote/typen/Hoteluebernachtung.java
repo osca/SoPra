53caa58de3330699a49d44c75e3ebcf5091e1920
Dennis Wiosna <dennis.wiosna@googlemail.com>
2012-02-28 11:50:25 +0100

    Auto Update

--- angebote/typen/Hoteluebernachtung.java
+++ angebote/typen/Hoteluebernachtung.java
@@ -16,4 +16,32 @@ public class Hoteluebernachtung extends Angebot {
 		super(pname, Angebot.HOTEL, ppreis, pdaten);
 	}
 	
+	public void setBeschreibung(String wert) {
+		erlaubteKriterien[BESCHREIBUNG].setWert(wert);
+	}
+	
+	public void setOrt(String wert) {
+		erlaubteKriterien[ORT].setWert(wert);
+	}
+	
+	public void setKlima(String wert) {
+		erlaubteKriterien[KLIMA].setWert(wert);
+	}
+	
+	public void setBetten(String wert) {
+		erlaubteKriterien[BETTEN].setWert(wert);
+	}
+	
+	public void setSterne(String wert) {
+		erlaubteKriterien[STERNE].setWert(wert);
+	}
+	
+	public void setVerpflegungsart(String wert) {
+		erlaubteKriterien[VERPFLEGUNGSART].setWert(wert);
+	}
+	
+	public void setBierpreis(String wert) {
+		erlaubteKriterien[BIERPREIS].setWert(wert);
+	}
+	
 }
