1ccf184122d57d2669e5550245400b2d0ca0da50
Dennis Wiosna <dennis.wiosna@googlemail.com>
2012-02-28 10:54:29 +0100

    Auto Update

--- angebote/typen/Ausfluege.java
+++ angebote/typen/Ausfluege.java
@@ -7,10 +7,18 @@ import angebote.kriterien.Ort;
 
 public class Ausfluege extends Angebot {
 	
-	Kriterium[] erlaubteKriterien;
+	Kriterium[] erlaubteKriterien = {new Ort(null),new };
 	
 	public Ausfluege(String pname, double ppreis, Date[] pdaten) {
 		super(pname, Angebot.AUSFLUG, ppreis, pdaten);
+	}
+	
+	private void main(String[] args) {
+		erlaubteKriterien[0].setWerte(werte);
+	}
+	
+	public void setOrt(ArrayList<String> werte) {
+		erlaubteKriterien[0].setWerte(werte);
 	}
 	
 }
