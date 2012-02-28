78162277a9b37946a79534114674bd040b7a6692
Stephan Övermöhle <s.o@wwu.de>
2012-02-28 12:15:28 +0100

    Nachricht

--- accounts/Nachricht.java
+++ accounts/Nachricht.java
@@ -1,16 +1,50 @@
 package accounts;
 
+import java.util.Date;
+
+import angebote.typen.Angebot;
+
+//@author Stephan
+
 public class Nachricht {
+	private Date zeitStempel;
 	private String text, betreff;
+	private Account absender, empfaenger;
+	private boolean gelesen;
+	public Account getAbsender() {
+		return absender;
+	}
+	public Account getEmpfaenger() {
+		return empfaenger;
+	}
+	public Angebot getAngebot() {
+		return angebot;
+	}
+	private Angebot angebot;
 	public String getText() {
 		return text;
 	}
 	public String getBetreff() {
 		return betreff;
 	}
-	public Nachricht(String subj, String txt){
+	public Date getZeitStempel() {
+		return zeitStempel;
+	}
+	public boolean isGelesen() {
+		return gelesen;
+	}
+	public void setGelesen(boolean gelesen) {
+		this.gelesen = gelesen;
+	}
+	public Nachricht(String subj, String txt, Account abs, Account empf, Angebot ang){
 		text=txt;
 		betreff=subj;
+		absender=abs;
+		empfaenger=empf;
+		angebot = ang;
+		gelesen = false;
+		zeitStempel = new Date();
 	}
+	
 	
 }
