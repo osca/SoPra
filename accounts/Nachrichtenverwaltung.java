ec405267737d2ebec4e62c97c309a36bb261984e
J <Jay.Schneider@uni-muenster.de>
2012-02-28 14:36:42 +0100

    Nachrichtenverwaltung geupdated

--- accounts/Nachrichtenverwaltung.java
+++ accounts/Nachrichtenverwaltung.java
@@ -1,10 +1,41 @@
 package accounts;
 
+import java.util.ArrayList;
+
+import angebote.typen.Angebot;
+/**
+ * 
+ * @author Jay
+ *
+ */
 public class Nachrichtenverwaltung {
-	
-	public void sendeNachricht(Account absender, Account empfaegner, String betreff, String text){
-		Nachricht msg = new Nachricht(betreff, text);
-		empfaegner.addNachricht(msg);
+	/**
+	 * Sendet Nachricht mit 'text' und 'betreff' von 'absender' an 'empfaenger' und verweist auf das 'angebot'
+	 * @param absender
+	 * @param empfaenger
+	 * @param betreff
+	 * @param text
+	 * @param angebot
+	 */
+	public void sendeNachricht(Account absender, Account empfaenger, String betreff, String text, Angebot angebot){
+		Nachricht msg = new Nachricht(betreff, text, absender, empfaenger, angebot);
+		empfaenger.addNachricht(msg);
 		//absender.addNachricht(msg);	//TODO Postausgang??
+	}
+	
+	/**
+	 * @param acc Account mit zu löschender Nachricht
+	 * @param msg zu löschende Nachricht
+	 */
+	public void delNachricht(Account acc, Nachricht msg){
+		acc.delNachricht(msg);
+	}
+	
+	/**
+	 * @param acc ausgewählter Account
+	 * @return ArrayList von Nachrichten des Accounts
+	 */
+	public ArrayList<Nachricht> getNachrichten(Account acc){
+		return acc.getPosteingang();
 	}
 }
