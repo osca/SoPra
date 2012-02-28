8f8a2077bc63285a19cb9c3e222bf6452b346c6f
J <Jay.Schneider@uni-muenster.de>
2012-02-28 15:24:19 +0100

    Portalklasse grob implementiert

--- Portal.java
+++ Portal.java
@@ -1,5 +1,40 @@
+import accounts.Accountverwaltung;
+import accounts.Nachrichtenverwaltung;
+import angebote.Angebotsverwaltung;
+import buchungen.Buchungsverwaltung;
+import angebot.Angebotsverarbeitung;
 
 
 public class Portal {
 
+	private Accountverwaltung accverw = new Accountverwaltung();
+	private Angebotsverwaltung angebverw = new Angebotsverwaltung();
+	private Buchungsverwaltung buchverw = new Buchungsverwaltung();
+	private Nachrichtenverwaltung nachrverw = new Nachrichtenverwaltung();
+	private Datenhaltung daha = new Datenhaltung();
+	private Angebotsverarbeitung angebverar = new Angebotsverarbeitung();
+	
+	public Accountverwaltung getAccountverwaltung(){
+		return accverw;
+	}
+
+	public Angebotsverwaltung getAngebverw() {
+		return angebverw;
+	}
+
+	public Buchungsverwaltung getBuchverw() {
+		return buchverw;
+	}
+
+	public Nachrichtenverwaltung getNachrverw() {
+		return nachrverw;
+	}
+
+	public Datenhaltung getDaha() {
+		return daha;
+	}
+
+	public Angebotsverarbeitung getAngebverar() {
+		return angebverar;
+	}
 }
--- accounts/Accountverwaltung.java
+++ accounts/Accountverwaltung.java
@@ -1,42 +1,63 @@
 package accounts;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Accountverwaltung {
-	
-	private Vector<Account> accounts = new Vector<Account>();
+
+	private ArrayList<Anbieter> anbieter = new ArrayList<Anbieter>();
+	private ArrayList<Betreiber> betreiber = new ArrayList<Betreiber>();
+	private ArrayList<Kunde> kunden = new ArrayList<Kunde>();
 
 	public void createKunde(String email, String name, String password) throws AlreadyInUseException{
 		if (!isFreeEmail(email) || !isFreeName(name))
 			throw new AlreadyInUseException();
-		accounts.add(new Kunde(email, name, password));
+		kunden.add(new Kunde(email, name, password));
 	}
 
 	public void createAnbieter(String email, String name, String password) throws AlreadyInUseException{
 		if (!isFreeEmail(email) || !isFreeName(name))
 			throw new AlreadyInUseException();
-		accounts.add(new Anbieter(email, name, password));
+		anbieter.add(new Anbieter(email, name, password));
 	}
 	
 	public void createBetreiber(String email, String name, String password) throws AlreadyInUseException{
 		if (!isFreeEmail(email) || !isFreeName(name))
 			throw new AlreadyInUseException();
-		accounts.add(new Betreiber(email, name, password));
+		betreiber.add(new Betreiber(email, name, password));
 	}
 	
 	public void setEnableAccount(Account acc, boolean enable){
 		acc.setGesperrt(enable);
 	}
 	
-	public void delAccount(Account acc){
-		boolean success = accounts.remove(acc);
-		if(!success)
-			return;		//TODO exception??
+	public boolean delAccount(Account acc){
+		boolean success[] = new boolean[3];
+		success[0] = getAnbieter().remove(acc);
+		success[1] = getBetreiber().remove(acc);
+		success[2] = getKunden().remove(acc);
+		return (success[0] || success[1] || success[2]);
+	}
+
+	public ArrayList<Anbieter> getAnbieter(){
+		return anbieter;
+	}
+	public ArrayList<Betreiber> getBetreiber(){
+		return betreiber;
+	}
+	public ArrayList<Kunde> getKunden(){
+		return kunden;
+	}
+	public ArrayList<Account> getAccounts(){
+		ArrayList<Account> result = new ArrayList<Account>();
+		result.addAll(getAnbieter());
+		result.addAll(getBetreiber());
+		result.addAll(getKunden());
+		return result;
 	}
 	
 	private boolean isFreeEmail(String email) {
 		boolean result = true;
-		for(Account a : accounts)
+		for(Account a : getAccounts())
 			if(a.getEmail().equals(email))
 				result = false;
 		return result;
@@ -44,7 +65,7 @@ public class Accountverwaltung {
 
 	private boolean isFreeName(String name) {
 		boolean result = true;
-		for(Account a : accounts)
+		for(Account a : getAccounts())
 			if(a.getName().equals(name))
 				result = false;
 		return result;
