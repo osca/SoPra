7e0e22832d82f7f419690b7f89cf1cdb3df9ba64
RudiX <ermantraut.rudolf@googlemail.com>
2012-02-29 15:32:57 +0100

    R

--- graphic/AngebotCreate.java
+++ graphic/AngebotCreate.java
@@ -1,8 +1,28 @@
 package graphic;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 public class AngebotCreate {
 
+	private JPanel up;
+	private JPanel sub_a;
+	private JPanel sub_b;
+	private JPanel mid;
+	private JPanel down;
 	
+	private JLabel name;
+	private JTextField name_e;
+	private JLabel typ;
+	private JComboBox typ_e;
+	private JLabel datum;
+	
+	private JLabel anbieter;
+	
+	private JLabel datum;
+	private JLabel anbieter;
+	
+	private JComboBox krit1;
 	
 }
