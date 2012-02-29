939f385388c6837e41cf185f5a27d18f279a6ab8
Jay Schneider <jay@jay-laptop.(none)>
2012-02-29 13:51:11 +0100

    LoeschenNichtMoeglichException edited

--- accounts/LoeschenNichtMoeglichException.java
+++ accounts/LoeschenNichtMoeglichException.java
@@ -4,4 +4,10 @@ public class LoeschenNichtMoeglichException extends Exception {
 
 	private static final long serialVersionUID = 5648913248746578922L;
 
+	public LoeschenNichtMoeglichException(){
+		super();
+	}
+	public LoeschenNichtMoeglichException(String s){
+		super(s);
+	}
 }
