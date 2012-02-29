11ecd5f33385d6cedc4b2e333679ebabbeabe5a9
RudiX <ermantraut.rudolf@googlemail.com>
2012-02-29 16:48:56 +0100

    R

--- graphic/AngebotCreate.java
+++ graphic/AngebotCreate.java
@@ -1,25 +1,58 @@
 package graphic;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
-public class AngebotCreate {
-
+public class AngebotCreate extends JPanel{
+	//b1.setToolTipText("Click this button to disable the middle button.");
 	private JPanel up;
 	private JPanel sub_a;
 	private JPanel sub_b;
 	private JPanel mid;
 	private JPanel down;
 	
-	private JLabel name;
-	private JTextField name_e;
-	private JLabel typ;
-	private JComboBox typ_e;
-	private JLabel datum;
+	private JTextField name;
+	private JComboBox typ;
+	//private Shice date ---> vllt.
+	private JTextField preis;
+	private JTextField kap;
 	private JLabel anbieter;
-	private JComboBox krit1;
+	
+	//Krit dropdown
+	
+	private JTextField beschreibung;
+	
+	private JButton verwerfen;
+	private JButton bestaetigen;
+	
+	public AngebotCreate(){
+		setLayout(new BorderLayout(5,5));
+		
+		up = new JPanel(new GridLayout(2,0));
+		sub_a = new JPanel(new GridLayout(6,0));
+		//sub_b = new JPanel(new GridLayout(6,0)); bei krits
+		
+		mid = new JPanel(new GridLayout(1,0));
+		beschreibung = new JTextField();
+		mid.add(beschreibung);
+		
+		down = new JPanel(new BorderLayout(5,5));
+		verwerfen = new JButton("Verwerfen");
+		down.add(BorderLayout.EAST, verwerfen);
+		bestaetigen = new JButton("Bestätigen");
+		down.add(BorderLayout.WEST, bestaetigen);
+		
+		add(BorderLayout.NORTH, up);
+		add(BorderLayout.CENTER, mid);
+		add(BorderLayout.SOUTH, down);
+		setVisible(true);
+	}
 	
 }
