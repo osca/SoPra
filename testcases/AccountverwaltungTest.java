417004f495d48f9c8d93eb9815c770bad24aa6fa
bennii <benjamin_bockhoefer@freenet.de>
2012-03-01 13:03:53 +0100

    accountverwaltungTest_mehrcases

--- testcases/AccountverwaltungTest.java
+++ testcases/AccountverwaltungTest.java
@@ -13,61 +13,75 @@ public class AccountverwaltungTest {
 	@BeforeClass
 	public static void setup() {
 		acv = Portal.getSingletonObject().getAccountverwaltung();
-		
-		try {
-			acv.createAnbieter("tui@flug.de", "TUI", "supadupa");
-			acv.createBetreiber("betreiber@host.de", "Horst", "ichkannalles");
-			acv.createKunde("nixda@dasda.de", "Ninja", "wurstloch");
-		} catch (AlreadyInUseException e) {
-			e.printStackTrace();
-		}
-		
 	}
 	
 	@Test
 	public void testCreateKunde() {
-		ArrayList<Kunde> kunden = acv.getKunden();
+		try {
+			acv.createKunde("nixda@dasda.de", "Ninja", "wurstloch");
+			
+			ArrayList<Kunde> kunden = acv.getKunden();
+			Assert.assertEquals(1, kunden.size());									// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?Assert.assertEquals(1, kunden.size());									// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?
+			
+			Kunde kunde = kunden.get(0);
+			Assert.assertEquals("Ninja", kunde.getName());
+			Assert.assertEquals("nixda@dasda.de", kunde.getEmail());
+			Assert.assertEquals("wurstloch", kunde.getPassword());
+		} catch (AlreadyInUseException e) {
+			e.printStackTrace();
+		}
 		
-		Assert.assertEquals(1, kunden.size());									// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?Assert.assertEquals(1, kunden.size());									// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?
 		
-		Kunde kunde = kunden.get(0);
-
-		Assert.assertEquals("Ninja", kunde.getName());
-		Assert.assertEquals("nixda@dasda.de", kunde.getEmail());
-		Assert.assertEquals("wurstloch", kunde.getPassword());
 	}
 	
 	@Test
 	public void testCreateAnbieter() {
-		ArrayList<Anbieter> anbieter = acv.getAnbieter();
-		
-		Assert.assertEquals(1, anbieter.size());								// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?
-		
-		Anbieter bieter = anbieter.get(0);
-		
-		Assert.assertEquals("TUI", bieter.getName());
-		Assert.assertEquals("tui@flug.de", bieter.getEmail());
-		Assert.assertEquals("supadupa", bieter.getPassword());
+		try {
+			acv.createAnbieter("tui@flug.de", "TUI", "supadupa");
+			
+			ArrayList<Anbieter> anbieter = acv.getAnbieter();
+			Assert.assertEquals(1, anbieter.size());								// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?
+			
+			Anbieter bieter = anbieter.get(0);
+			
+			Assert.assertEquals("TUI", bieter.getName());
+			Assert.assertEquals("tui@flug.de", bieter.getEmail());
+			Assert.assertEquals("supadupa", bieter.getPassword());
+		} catch (AlreadyInUseException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void testeCreateBetreiber() {
-		ArrayList<Betreiber> betreiber = acv.getBetreiber();
 		
-		Assert.assertEquals(1, betreiber.size());								// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?7
-		
-		Betreiber treiber = betreiber.get(0);
+		try {
+			acv.createBetreiber("betreiber@host.de", "Horst", "ichkannalles");
+			
+			ArrayList<Betreiber> betreiber = acv.getBetreiber();
+			Assert.assertEquals(1, betreiber.size());								// habe ich auch wirklich nur einen erstellt, wenn nicht fehler im setup?7
+			
+			Betreiber treiber = betreiber.get(0);
 
-		Assert.assertEquals("Horst", treiber.getName());
-		Assert.assertEquals("betreiber@host.de", treiber.getEmail());
-		Assert.assertEquals("ichkannalles", treiber.getPassword());
+			Assert.assertEquals("Horst", treiber.getName());
+			Assert.assertEquals("betreiber@host.de", treiber.getEmail());
+			Assert.assertEquals("ichkannalles", treiber.getPassword());
+		} catch (AlreadyInUseException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void testEnableAccount() {
-		acv.setEnableAccount(acv.getKunden().get(0), false);
+		try {
+			acv.createKunde("nixda@dasda.de", "Ninja", "wurstloch");
+			acv.setEnableAccount(acv.getKunden().get(0), false);
+			
+			Assert.assertEquals(acv.getKunden().get(0).isGesperrt(), false);
+		} catch (AlreadyInUseException e) {
+			e.printStackTrace();
+		}
 		
-		Assert.assertEquals(acv.getKunden().get(0).isGesperrt(), false);
 	}
 	
 	 @Test
