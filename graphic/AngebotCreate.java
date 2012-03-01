14824dc6b544050f1b98ecd8e1a7062179e303e9
RudiX <ermantraut.rudolf@googlemail.com>
2012-03-01 15:52:10 +0100

    R newAngDet shice all

--- graphic/AngebotCreate.java
+++ graphic/AngebotCreate.java
@@ -19,28 +19,29 @@ import angebote.kriterien.Bierpreis;
 import angebote.kriterien.Klasse;
 import angebote.kriterien.Klima;
 import angebote.kriterien.Verpflegungsart;
+import angebote.typen.Angebot;
+import angebote.typen.Flug;
 import angebote.typen.Hoteluebernachtung;
 
-
-public class AngebotCreate extends JPanel implements ActionListener{
-	//b1.setToolTipText("Click this button to disable the middle button.");
+public class AngebotCreate extends JPanel implements ActionListener {
+	// b1.setToolTipText("Click this button to disable the middle button.");
 	private JPanel up;
 	private JPanel sub_a;
 	private JPanel sub_b;
 	private JPanel mid;
 	private JPanel down;
-	
+
 	private JTextField name;
-	
+
 	private JComboBox typ;
 	private Vector<String> typ_l;
 	private DefaultComboBoxModel typ_m;
-	//private Shice date ---> vllt.
+	// private Shice date ---> vllt.
 	private JTextField preis;
 	private JTextField kap;
 	private JLabel anbieter;
-	
-	//Krit dropdown
+
+	// Krit dropdown
 	private JTextField ort;
 	private JTextField ortz;
 	private JComboBox klima;
@@ -49,23 +50,22 @@ public class AngebotCreate extends JPanel implements ActionListener{
 	private JComboBox bierpreis;
 	private JComboBox klasse;
 	private String[] k;
-	
+
 	private JTextArea beschreibung;
-	
+
 	private JButton verwerfen;
 	private JButton bestaetigen;
-	
-	public AngebotCreate(Anbieter a){
-		setLayout(new BorderLayout(5,5));
-		
-		up = new JPanel(new GridLayout(2,0));
-		sub_a = new JPanel(new GridLayout(6,0));
+
+	public AngebotCreate(Anbieter a) {
+		setLayout(new BorderLayout(5, 5));
+
+		up = new JPanel(new GridLayout(2, 0));
+		sub_a = new JPanel(new GridLayout(6, 0));
 		name = new JTextField();
 		name.setToolTipText("Bitte Namen eingeben");
 		sub_a.add(name);
-		
-		
-		typ_l= new Vector<String>();
+
+		typ_l = new Vector<String>();
 		typ_l.add("Typ");
 		typ_l.add("Hoteluebernachtung");
 		typ_l.add("Autovermietung");
@@ -74,107 +74,99 @@ public class AngebotCreate extends JPanel implements ActionListener{
 		typ = new JComboBox(typ_l);
 		typ.setToolTipText("Bitte waehlen Sie eine Typ aus");
 		typ.addActionListener(this);
-		
+
 		sub_a.add(typ);
-		preis= new JTextField();
+		preis = new JTextField();
 		sub_a.add(preis);
 		kap = new JTextField();
 		sub_a.add(kap);
 		anbieter = new JLabel(a.getName());
 		sub_a.add(anbieter);
-		sub_b = new JPanel(new GridLayout(6,0));
+		sub_b = new JPanel(new GridLayout(6, 0));
 		up.add(sub_a);
 		up.add(sub_b);
-		
-		mid = new JPanel(new GridLayout(1,0));
+
+		mid = new JPanel(new GridLayout(1, 0));
 		beschreibung = new JTextArea();
 		beschreibung.setEditable(true);
 		mid.add(beschreibung);
-		
-		down = new JPanel(new BorderLayout(5,5));
+
+		down = new JPanel(new BorderLayout(5, 5));
 		verwerfen = new JButton("Verwerfen");
 		down.add(BorderLayout.EAST, verwerfen);
 		bestaetigen = new JButton("Bestätigen");
 		down.add(BorderLayout.WEST, bestaetigen);
-		
+
 		add(BorderLayout.NORTH, up);
 		add(BorderLayout.CENTER, mid);
 		add(BorderLayout.SOUTH, down);
-		setVisible(true);	
+		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		/**
-		 * to do : dynamische erzeugung mit add-methode und of else über erlaubte kriterien
-		 */
-		if(e.getSource()==typ_l.elementAt(0)){
-			sub_b.removeAll();
-			sub_b.repaint();
-		}
-		
-		if(e.getSource()==typ_l.elementAt(1)){
-			ort = new JTextField();
-			ort.setToolTipText("Bitte geben SIe einen Ort");
-			sub_b.add(ort);
-			
-			
-			klima = new JComboBox(Klima.wertebereich);
-			sub_b.add(klima);
-			
-			sterne = new JTextField();
-			sterne.setToolTipText("Bitte geben Sie Anzahl der Sterne ein");
-			sub_b.add(sterne);
-			
-			
-			verpflegung = new JComboBox(Verpflegungsart.wertebereich);
-			sub_b.add(verpflegung);
 
-			bierpreis =  new JComboBox(Bierpreis.wertebereich);
-			sub_b.add(bierpreis);
-			sub_b.repaint();
-		}
-		
-		if(e.getSource()==typ_l.elementAt(2)){
-			ort = new JTextField();
-			ort.setToolTipText("Bitte geben SIe einen Ort");
-			sub_b.add(ort);
-			sub_b.repaint();
-		}
-		
-		if(e.getSource()==typ_l.elementAt(3)){
-			ort = new JTextField();
-			ort.setToolTipText("Bitte geben SIe einen Ort");
-			sub_b.add(ort);
-		
-			bierpreis =  new JComboBox(Bierpreis.wertebereich);
-			sub_b.add(bierpreis);
-			sub_b.repaint();
-		}
-		
-		if(e.getSource()==typ_l.elementAt(4)){
-			ort = new JTextField();
-			ort.setToolTipText("Bitte geben SIe einen Startort");
-			sub_b.add(ort);
-			
-			ortz = new JTextField();
-			ortz.setToolTipText("Bitte geben SIe einen Zielort");
-			sub_b.add(ortz);
-			
-			klasse = new JComboBox(Klasse.wertebereich);
-			sub_b.add(klasse);
-			
-			bierpreis =  new JComboBox(Bierpreis.wertebereich);
-			sub_b.add(bierpreis);
-			sub_b.repaint();
+		if (e.getSource() == typ) {
+
+			if (typ.getSelectedItem().toString() == typ_l.elementAt(0)) {
+				sub_b.removeAll();
+				sub_b.repaint();
+			}
+
+			if (typ.getSelectedItem().toString() == typ_l.elementAt(1)) {
+				ort = new JTextField();
+				ort.setToolTipText("Bitte geben SIe einen Ort");
+				sub_b.add(ort);
+
+				klima = new JComboBox(Klima.wertebereich);
+				sub_b.add(klima);
+
+				sterne = new JTextField();
+				sterne.setToolTipText("Bitte geben Sie Anzahl der Sterne ein");
+				sub_b.add(sterne);
+
+				verpflegung = new JComboBox(Verpflegungsart.wertebereich);
+				sub_b.add(verpflegung);
+
+				bierpreis = new JComboBox(Bierpreis.wertebereich);
+				sub_b.add(bierpreis);
+				sub_b.repaint();
+			}
+
+			if (typ.getSelectedItem().toString() == typ_l.elementAt(2)) {
+				ort = new JTextField();
+				ort.setToolTipText("Bitte geben SIe einen Ort");
+				sub_b.add(ort);
+				sub_b.repaint();
+			}
+
+			if (typ.getSelectedItem().toString() == typ_l.elementAt(3)) {
+				ort = new JTextField();
+				ort.setToolTipText("Bitte geben SIe einen Ort");
+				sub_b.add(ort);
+
+				bierpreis = new JComboBox(Bierpreis.wertebereich);
+				sub_b.add(bierpreis);
+				sub_b.repaint();
+			}
+
+			if (typ.getSelectedItem().toString() == typ_l.elementAt(4)) {
+				ort = new JTextField();
+				ort.setToolTipText("Bitte geben Sie einen Startort");
+				sub_b.add(ort);
+
+				ortz = new JTextField();
+				ortz.setToolTipText("Bitte geben SIe einen Zielort");
+				sub_b.add(ortz);
+
+				klasse = new JComboBox(Klasse.wertebereich);
+				sub_b.add(klasse);
+
+				bierpreis = new JComboBox(Bierpreis.wertebereich);
+				sub_b.add(bierpreis);
+				sub_b.repaint();
+			}
 		}
 	}
-	
-	public void addShice(String fu){
-		if(fu=="Hoteluebernachtung"){
-			k=Hoteluebernachtung.getErlaubteKriterien();
-		}
-	}
-		
+
 }
