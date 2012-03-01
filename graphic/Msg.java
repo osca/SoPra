c355346e5104026aeaa622545909413f1c9116c5
bennii <benjamin_bockhoefer@freenet.de>
2012-03-01 17:31:07 +0100

    msg_sendenlogik_added

--- graphic/Msg.java
+++ graphic/Msg.java
@@ -1,39 +1,56 @@
 package graphic;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import accounts.*;
+import angebote.typen.Angebot;
+import main.Portal;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
-import accounts.Account;
 
-public class Msg extends JDialog implements ActionListener{
+/**
+ * @author Rudi
+ * @edit : Benjamin
+ */
+public class Msg extends JDialog {
 	private JLabel absender;
 	private JLabel empfaenger;
-	
 	private JLabel betreff;
+	
 	private JTextArea m;
+	
 	private JPanel up;
 	private JPanel mid;
 	private JPanel down;
+	
 	private JButton senden;
 	private JButton verwerfen;
 	
 	private String msgText;
 	
-	public Msg(Account acc, Account ziel, String b){
+	// added bei Benjamin
+	private Account sender;
+	private Account empf;
+	private String betr;
+	private Angebot bezug;
+	
+	// added bei benjamin: Angebot angebot
+	public Msg(Account acc, Account ziel, String b, Angebot angebot){
+		sender = acc;
+		empf = ziel;
+		betr = b;
+		bezug = angebot;
 		// to do set size
-		setDefaultCloseOperation(this.DO_NOTHING_ON_CLOSE);
-		setLayout(new BorderLayout (5,5));
-		up = new JPanel(new GridLayout (3,0));
-		mid = new JPanel(new BorderLayout (5,5));
-		down = new JPanel(new BorderLayout (5,5));
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		setLayout(new BorderLayout (5, 5));
+		setPreferredSize(new Dimension(500, 500));
+		
+		up = new JPanel(new GridLayout (3, 0));
+		mid = new JPanel(new BorderLayout (5, 5));
+		down = new JPanel(new BorderLayout (5, 5));
 		
 		absender = new JLabel(acc.getName());
 		up.add(absender);
@@ -41,39 +58,42 @@ public class Msg extends JDialog implements ActionListener{
 		up.add(empfaenger);
 		betreff = new JLabel(b);
 		up.add(betreff);
-		
 
 		m = new JTextArea();
 		mid.add(m);
 		
 		senden = new JButton("Senden");
-		senden.addActionListener(this);
+		senden.addActionListener(sendenListener);
 		verwerfen = new JButton ("Verwerfen");
-		verwerfen.addActionListener(this);
+		verwerfen.addActionListener(verwerfenListener);
 		down.add(BorderLayout.EAST, senden);
 		down.add(BorderLayout.WEST, verwerfen);
 		
 		add(BorderLayout.NORTH, up);
 		add(BorderLayout.CENTER, mid);
 		add(BorderLayout.SOUTH, down);
+		
 		setVisible(true);
 		setAlwaysOnTop(true);
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==senden){
-			msgText = m.getText();
-		}
-		else if(e.getSource()==verwerfen){
-			msgText = null;
-			hide();
-		}
-		
 	}
 	
 	public String getText(){
 		return msgText;
 	}
+	
+	ActionListener sendenListener = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Portal.getSingletonObject().getNachrichtenverwaltung().sendeNachricht(sender, empf, betr, msgText, bezug);
+		}
+	};
+	
+	ActionListener verwerfenListener = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			dispose();
+		}
+	};
 }
