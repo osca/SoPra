c720fdb4603a2baed9d5eaa41d8255e9a8233e60
Dennis Wiosna <dennis.wiosna@googlemail.com>
2012-03-01 11:05:15 +0100

    AngebotTest

---
+++ testcases/AngebotTest.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AngebotTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
