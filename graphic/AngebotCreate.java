6083d03dde1d6b16f3eef6721c9f8d199f4325e1
RudiX <ermantraut.rudolf@googlemail.com>
2012-03-01 17:55:23 +0100

    R

--- graphic/AngebotCreate.java
+++ graphic/AngebotCreate.java
@@ -32,7 +32,7 @@ public class AngebotCreate extends JPanel implements ActionListener {
 	private JTextField name;
 
 	private JComboBox typ;
-	private Vector<String> typ_l;
+	private Vector<String> typ_list;
 	private DefaultComboBoxModel typ_m;
 	// private Shice date ---> vllt.
 	private JTextField preis;
@@ -64,25 +64,33 @@ public class AngebotCreate extends JPanel implements ActionListener {
 		sub_a.add(name_label);
 		name.setToolTipText("Bitte Namen eingeben");
 		sub_a.add(name);
-
-		typ_l = new Vector<String>();
-		typ_l.add("Typ");
-		typ_l.add("Hoteluebernachtung");
-		typ_l.add("Autovermietung");
-		typ_l.add("Ausflueg");
-		typ_l.add("Flug");
-		typ = new JComboBox(typ_l);
+		
+		JLabel typ_label = new JLabel("Typ:");
+		sub_a.add(typ_label);
+		typ_list = new Vector<String>();
+		typ_list.add("Typ");
+		typ_list.add("Hoteluebernachtung");
+		typ_list.add("Autovermietung");
+		typ_list.add("Ausflueg");
+		typ_list.add("Flug");
+		typ = new JComboBox(typ_list);
 		typ.setToolTipText("Bitte waehlen Sie eine Typ aus");
 		typ.addActionListener(this);
 
 		sub_a.add(typ);
+		JLabel preis_label = new JLabel("Preis:");
+		sub_a.add(preis_label);
 		preis = new JTextField();
 		sub_a.add(preis);
+		JLabel kap_label = new JLabel("Kapazitaet:");
+		sub_a.add(kap_label);
 		kap = new JTextField();
 		sub_a.add(kap);
+		JLabel anbieter_label = new JLabel("Anbieter:");
+		sub_a.add(anbieter_label);
 		anbieter = new JLabel(a.getName());
 		sub_a.add(anbieter);
-		sub_b = new JPanel(new GridLayout(6, 0));
+		sub_b = new JPanel(new GridLayout(6, 2));
 		up.add(sub_a);
 		up.add(sub_b);
 
@@ -108,59 +116,80 @@ public class AngebotCreate extends JPanel implements ActionListener {
 
 		if (e.getSource() == typ) {
 			sub_b.removeAll();
-			if (typ.getSelectedItem().toString() == typ_l.elementAt(0)) {
+			if (typ.getSelectedItem().toString() == typ_list.elementAt(0)) {
 				
 			}
 
-			if (typ.getSelectedItem().toString() == typ_l.elementAt(1)) {
+			if (typ.getSelectedItem().toString() == typ_list.elementAt(1)) {
+				JLabel ort_label = new JLabel("Ort:");
+				sub_b.add(ort_label);
 				ort = new JTextField();
 				ort.setToolTipText("Bitte geben Sie einen Ort");
 				sub_b.add(ort);
-
+				
+				JLabel klima_label= new JLabel("Klima:");
+				sub_b.add(klima_label);
 				klima = new JComboBox(Klima.wertebereich);
 				sub_b.add(klima);
-
+				
+				JLabel sterne_label= new JLabel("Sterne:");
+				sub_b.add(sterne_label);
 				sterne = new JTextField();
 				sterne.setToolTipText("Bitte geben Sie Anzahl der Sterne ein");
 				sub_b.add(sterne);
-
+				
+				JLabel verpfelgeung_label= new JLabel("Verpflegung:");
+				sub_b.add(verpfelgeung_label);
 				verpflegung = new JComboBox(Verpflegungsart.wertebereich);
 				sub_b.add(verpflegung);
-
+				
+				JLabel bierpreis_label= new JLabel("Bierpreie:");
+				sub_b.add(bierpreis_label);
 				bierpreis = new JComboBox(Bierpreis.wertebereich);
 				sub_b.add(bierpreis);
 				
 			}
 
-			if (typ.getSelectedItem().toString() == typ_l.elementAt(2)) {
+			if (typ.getSelectedItem().toString() == typ_list.elementAt(2)) {
 				ort = new JTextField();
 				ort.setToolTipText("Bitte geben SIe einen Ort");
 				sub_b.add(ort);
 			
 			}
 
-			if (typ.getSelectedItem().toString() == typ_l.elementAt(3)) {
+			if (typ.getSelectedItem().toString() == typ_list.elementAt(3)) {
+				JLabel ort_label= new JLabel("Ort:");
+				sub_b.add(ort_label);
 				ort = new JTextField();
 				ort.setToolTipText("Bitte geben SIe einen Ort");
 				sub_b.add(ort);
-
+				
+				JLabel bierpreis_label= new JLabel("Bierpreis:");
+				sub_b.add(bierpreis_label);
 				bierpreis = new JComboBox(Bierpreis.wertebereich);
 				sub_b.add(bierpreis);
 				
 			}
 
-			if (typ.getSelectedItem().toString() == typ_l.elementAt(4)) {
+			if (typ.getSelectedItem().toString() == typ_list.elementAt(4)) {
+				JLabel ort_label= new JLabel("Startort:");
+				sub_b.add(ort_label);
 				ort = new JTextField();
 				ort.setToolTipText("Bitte geben Sie einen Startort");
 				sub_b.add(ort);
-
+				JLabel ortz_label= new JLabel("Zielort:");
+				sub_b.add(ortz_label);
 				ortz = new JTextField();
 				ortz.setToolTipText("Bitte geben Sie einen Zielort");
 				sub_b.add(ortz);
-
+				
+				JLabel klasse_label= new JLabel("Klasse:");
+				sub_b.add(klasse_label);
 				klasse = new JComboBox(Klasse.wertebereich);
 				sub_b.add(klasse);
-
+				
+				JLabel bierpreis_label= new JLabel("Bierpreie:");
+				sub_b.add(bierpreis_label);
 				bierpreis = new JComboBox(Bierpreis.wertebereich);
 				sub_b.add(bierpreis);
 				
