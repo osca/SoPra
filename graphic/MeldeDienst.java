2f291d12f846013315dd29f2e8e3f5d802a0105d
Jayronic <Jay.Schneider@uni-muenster.de>
2012-03-05 12:22:15 +0100

    umlaut in MSG entfernt

--- graphic/MeldeDienst.java
+++ graphic/MeldeDienst.java
@@ -2,12 +2,12 @@ package graphic;
 
 public class MeldeDienst
 {
-	protected static final String MSG_LOGIN_FEHLT = "Sie m�ssen sich einlogen um weitere Aktionen durchf�hren zu k�nnen";
-	protected static final String QSN_ANGEBOT_MELDEN	= "Sind Sie sicher, dass sie dieses Angebot Melden m�chten?";
+	protected static final String MSG_LOGIN_FEHLT = "Sie muessen sich einlogen um weitere Aktionen durchfuehren zu koennen";
+	protected static final String QSN_ANGEBOT_MELDEN	= "Sind Sie sicher, dass sie dieses Angebot Melden moechten?";
 	protected static final String MSG_BESCHWERDE = "Beschwerde";
 	protected static final String MSG_ANGEBOT_GEMELDET = "Ein Kunde hat ein Angebot gemeldet!";
-	protected static final String QSN_BUCHEN = "M�chten Sie das Angebot wirklich Buchen?";
-	protected static final String MSG_AGB_ERKLAERUNG = "Erkl�rung der AGB des Anbieters: ";
+	protected static final String QSN_BUCHEN = "Moechten Sie das Angebot wirklich Buchen?";
+	protected static final String MSG_AGB_ERKLAERUNG = "Erklaerung der AGB des Anbieters: ";
 	protected static final String MSG_GESAMMT_BEWERUNG = "Gesammtberwertung dieses Anbieters: ";
 	
 }
