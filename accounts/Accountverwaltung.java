b68e4ac0c68caa660c011b74638d6d9cba6867b5
Jayronic <Jay.Schneider@uni-muenster.de>
2012-03-06 10:52:14 +0100

    AccVerw

--- accounts/Accountverwaltung.java
+++ accounts/Accountverwaltung.java
@@ -169,9 +169,10 @@ public class Accountverwaltung {
 		Account acc = getAccountByIdentifier(identifier);
 		if (acc == null)
 			throw new LoginException("Account wurde nicht gefunden");
+		if (acc.isGesperrt()) 
+			throw new LoginException("Sie sind gesperrt. Bitte wenden Sie sich an den Betreiber.");
 		if (!acc.getPassword().equals(acc.hashPassword(password)))
 			throw new LoginException("Passwort war falsch");
-		if (acc.isGesperrt()) throw new LoginException("Sie sind gesperrt. Bitte wenden Sie sich an den Betreiber.");
 		loggedIn = acc;
 	}
 
