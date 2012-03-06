c40387cced006ef8d06e6f4fd19e5aaf6b1d93a2
Jayronic <Jay.Schneider@uni-muenster.de>
2012-03-06 11:46:49 +0100

    getStringFromFile added to DaHa

--- main/Datenhaltung.java
+++ main/Datenhaltung.java
@@ -187,5 +187,18 @@ public class Datenhaltung {
 			s[i] = (String) reslist.get(i);
 		return s;
 	}
+	
+	/**
+	 * Gibt den Datei-Inhalt als einzelnen String aus. Zeilen werden mit \n getrennt
+	 * @param f File-Objekt als Referenz zur Datei
+	 * @return 
+	 */
+	public static String getStringFromFile(File f){
+		String arr[] = getStringArrayFromFile(f);
+		String res = "";
+		for(String s : arr)
+			res += s+"\n";
+		return res;
+	}
 
 }
