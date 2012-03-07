e452ca889461d6710931b3fa42307c846ef3c3d2
Stephan Övermöhle <s.o@wwu.de>
2012-03-07 18:36:36 +0100

    todo liste

--- main/Main.java
+++ main/Main.java
@@ -2,10 +2,7 @@ package main;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		Datenhaltung.recoverSavedState();
 		graphic.MainFrame frame = new graphic.MainFrame();
 		frame.setVisible(true);
