3112b7ad3d8c1b93082e7a53b785395d1930df14
RudiX <ermantraut.rudolf@googlemail.com>
2012-03-08 19:44:30 +0100

    BuchDetScreen

--- graphic/BuchDetailScreen.java
+++ graphic/BuchDetailScreen.java
@@ -7,7 +7,9 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -21,6 +23,7 @@ import main.Portal;
 import accounts.Account;
 import accounts.Anbieter;
 import accounts.Kunde;
+import angebote.kriterien.Kriterium;
 import angebote.typen.Angebot;
 import buchungen.Bestaetigung;
 import buchungen.Buchung;
@@ -54,52 +57,104 @@ public class BuchDetailScreen extends JPanel
 		
 		////////////
 		
-		JPanel labelPanel = new JPanel(new BorderLayout());
+		//JPanel labelPanel = new JPanel(new BorderLayout());
 		
-		north = new JPanel(new GridLayout(1,0));
+		north = new JPanel(new BorderLayout());
 		center = new JPanel(new GridLayout(1,0));
 		south = new JPanel(new GridLayout(1,0));
 		
 		north.setBorder(border);
 		center.setBorder(border);
 		
-		////////////
-		
-		JPanel left = new JPanel(grid);
-		final int length = 11;
-
-		JLabel[] labels = new JLabel[length];
-		labels[0] = new JLabel("  Name des Angebots: ");
-		labels[1] = new JLabel("  Kunde der gebucht: ");
-		labels[2] = new JLabel("  Angebotstyp: ");
-		labels[3] = new JLabel("  Beginndatum: ");
-		labels[4] = new JLabel("  Enddatum: ");
-		labels[5] = new JLabel("  Status der Buchung: ");
-		
-		for(int i=0; i<(labels.length+1)/2; i++)
-			left.add(labels[i]);
-		
-		///////
-		
-		JPanel right = new JPanel(grid);
 		Angebot angebot = Portal.Angebotsverwaltung().getAngebotByNummer(buchung.getAngebotsNummer());
 		Kunde kunde = Portal.Buchungsverwaltung().getKunde(buchung);
-
+		Anbieter anbieter = Portal.Angebotsverwaltung().getAnbieter(angebot);
 		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		DecimalFormat f = new DecimalFormat("#0.00"); 
 		
-		labels[6] = new JLabel(angebot.getName());
-		labels[7] = new JLabel(kunde.getName());
-		labels[8] = new JLabel(Angebot.convertTypToName(angebot.getTyp()));
-		labels[9] = new JLabel(formatter.format(angebot.getStartdatum()));
-		labels[10] = new JLabel(formatter.format(angebot.getStartdatum()));
+		JPanel sub_a = new JPanel(new GridLayout(0,2));
+		JPanel sub_one = new JPanel(new GridLayout(10,1));
+		JPanel sub_two = new JPanel(new GridLayout(10,1));
+		int length_left = 10;
+		JLabel [] sub_one_labels = new JLabel[length_left];
+		sub_one_labels[0] = new JLabel("Angebotsnummer:");
+		sub_one_labels[1] = new JLabel("Angebotsname:");
+		sub_one_labels[2] = new JLabel("Angebotswertung:");
+		sub_one_labels[3] = new JLabel("Anbieter:");
+		sub_one_labels[4] = new JLabel("Anbieterwertung:");
+		sub_one_labels[5] = new JLabel("Typ:");
+		sub_one_labels[6] = new JLabel("Startdatum der Buchung:");
+		sub_one_labels[7] = new JLabel("Startdatum der Buchung:");
+		sub_one_labels[8] = new JLabel("Gebucht von:");
+		sub_one_labels[9] = new JLabel("Preis");
 		
+		JLabel [] sub_two_labels = new JLabel[length_left];
+		sub_two_labels[0] = new JLabel(""+angebot.getAngebotsNummer());
+		sub_two_labels[1] = new JLabel(angebot.getName());
+		sub_two_labels[2] = new JLabel(""+f.format(angebot.getWertung()));
+		sub_two_labels[3] = new JLabel(angebot.getAnbieterName());
+		sub_two_labels[4] = new JLabel(anbieter.getName());
+		sub_two_labels[5] = new JLabel("" + Angebot.convertTypToName(angebot.getTyp()));
+		sub_two_labels[6] = new JLabel(formatter.format(buchung.getVon()));
+		sub_two_labels[7] = new JLabel(formatter.format(buchung.getBis()));
+		sub_two_labels[8] = new JLabel(buchung.getKundenName());
+		sub_two_labels[9] = new JLabel(""+f.format(angebot.getPreis()));
+		
+		for(int i=0;i<length_left;i++){
+			sub_one.add(sub_one_labels[i]);
+			sub_two.add(sub_two_labels[i]);
+		}
+		
+		JPanel sub_b = new JPanel(new GridLayout(0,2));
+		JPanel sub_1 = new JPanel(new GridLayout(8,1));
+		JPanel sub_2 = new JPanel(new GridLayout(8,1));
+		ArrayList<Kriterium> w = angebot.getKriterien();
+		for (int i = 0; i < w.size(); i++) {
+			JLabel krit = new JLabel(w.get(i).getName());
+			sub_1.add(krit);
+			JLabel krit1 = new JLabel(w.get(i).getWert());
+			sub_2.add(krit1);
+		}
+		
+		
+		
+//		////////////
+//		JPanel left = new JPanel(grid);
+//		final int length = 11;
+//
+//		JLabel[] labels = new JLabel[length];
+//		labels[0] = new JLabel("  Name des Angebots: ");
+//		labels[1] = new JLabel("  Angebotstyp: ");
+//		labels[2] = new JLabel("  Kunde der gebucht: ");
+//		labels[3] = new JLabel("  Beginndatum: ");
+//		labels[4] = new JLabel("  Enddatum: ");
+//		labels[5] = new JLabel("  Status der Buchung: ");
+//		
+//		for(int i=0; i<(labels.length+1)/2; i++)
+//			left.add(labels[i]);
+//		
+//		///////
+//		
+//		JPanel right = new JPanel(grid);
+//		
+//		
+//		labels[6] = new JLabel(angebot.getName());
+//		labels[7] = new JLabel(kunde.getName());
+//		labels[8] = new JLabel(Angebot.convertTypToName(angebot.getTyp()));
+//		labels[9] = new JLabel(formatter.format(angebot.getStartdatum()));
+//		labels[10] = new JLabel(formatter.format(angebot.getStartdatum()));
+//		
 		final JLabel status = new JLabel(buchung.getStatus());
+		JLabel status_label = new JLabel("Status:");
+		sub_1.add(status_label);
+		sub_2.add(status);
 		
-		for(int i=(length+1)/2; i<labels.length; i++)
-			right.add(labels[i]);
-		right.add(status);
-		
-		////////////
+//		
+//		for(int i=(length+1)/2; i<labels.length; i++)
+//			right.add(labels[i]);
+//		right.add(status);
+//		
+//		////////////
 		
 		fullinfo = new JTextArea(Portal.Buchungsverwaltung().getReferringAngebot(buchung).getFullInfo());
 		fullinfo.setEditable(false);
@@ -226,21 +281,25 @@ public class BuchDetailScreen extends JPanel
 		});
 		
 		////////////
+		sub_a.add(sub_1);
+		sub_a.add(sub_2);
+		sub_b.add(sub_one);
+		sub_b.add(sub_two);
+		JPanel temp = new JPanel(new FlowLayout());
 
-		north.add(left, BorderLayout.WEST);
-		north.add(right, BorderLayout.EAST);
-		
-		labelPanel.add(north,BorderLayout.WEST);
+		north.add(sub_a,BorderLayout.EAST);
+		north.add(sub_b,BorderLayout.CENTER);
+		temp.add(north);
+//		labelPanel.add(north,BorderLayout.WEST);
 		JPanel button_panel = new JPanel(new FlowLayout());
 		button_panel.add(buttonLinks);
 		button_panel.add(buttonRechts);
 		button_panel.add(buttonAngebot);
-//		south.add(buttonLinks);
 //		south.add(buttonRechts);
 //		south.add(buttonAngebot);
 		south.add(button_panel,BorderLayout.CENTER);
 		
-		this.add(labelPanel, BorderLayout.NORTH);
+		this.add(temp, BorderLayout.NORTH);
 		this.add(center, BorderLayout.CENTER);
 		this.add(south, BorderLayout.SOUTH);
 	}
